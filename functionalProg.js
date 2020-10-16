69296cd4537c2d582f9df24576b558fb347c86a6
Siamak Kosari <skosari@protonmail.com>
2020-10-16 13:13:47 -0700

    Mean Median Mode functional programing example

---
+++ functionalProg.js
@@ -0,0 +1,16 @@
+//Mean Median Mode
+function getMean(arr) {
+
+}
+
+function getMedian(arr) {
+
+}
+
+function getMode(arr) {
+
+}
+
+function meanMedianMode(arr) {
+
+}
--- reverseArray.js
+++ reverseArray.js
@@ -4,10 +4,26 @@ function reverseArrayInPlace(arr) {
   for(let i = 0; i < arr.length / 2; i++) {
     let temp = arr[i];
     arr[i] = arr[arr.length - 1 - i];
-    console.log(arr.length-1-i);
     arr[arr.length - 1 - i] = temp;
   }
   console.log(arr);
 }
 
 reverseArrayInPlace([1,2,3,4,5,'word', 'another-word',8, 9])
+
+function reverseArrayEasy(arr) {
+  arr.reverse();
+  console.log(arr)
+}
+
+reverseArrayEasy([1,2,3,4,5,'word', 'another-word',8, 9])
+
+function reverseArrayManual(arr) {
+  let newArr = [];
+  for (let i=0; i < arr.length; i++) {
+    newArr.push(arr[arr.length - 1 - i]);
+  }
+  console.log(newArr);
+}
+
+reverseArrayManual([1,2,3,4,5,'word', 'another-word',8, 9])
