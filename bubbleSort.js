3d17c830c367866e0ef2e2d6b74e5179e25503ef
skosari <skosari@protonmail.com>
2020-10-18 14:26:33 -0700

    Merged bs

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,5 @@
-
-
-
-
+//compare each pair of numbers from start to finish then order thast individual pair - in then the largest number will go to the end as it "bubbles its way up"
+// # of passes array.length - 1
 
 function bubbleSort (array) {
   for (var i = array.length; i > 0; i--) {
