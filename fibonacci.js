15251e56fd5b82d72c96a9343c71ef4190791ece
skosari <skosari@protonmail.com>
2020-10-18 10:11:47 -0700

    SofE

--- fibonacci.js
+++ fibonacci.js
@@ -6,7 +6,7 @@ function fibonacciExpo(position) {
   if (position < 3) return 1  //Base case
   else return fibonacci(position -1) + fibonacci(position -2)
 }
-console.log(fibonacciExpo(6))
+//console.log(fibonacciExpo(6))
 //fib(6)
 //fib(5) +          fib(4)
 //fib(4) +          fib(3) + fib(3) + fib(2)
@@ -14,13 +14,13 @@ console.log(fibonacciExpo(6))
 //  2    +   1    +   2    +   2    +   1    = 8
 
 //WAY faster
-function fibonacci (position) {
+function fibonacci(position) {
   if (position < 3) {
     return 1;
   }
   var arr =[1,1];
   var sum = 1;
-  for (i =0; i < position; i++) {
+  for (i=0; i < position; i++) {
       sum = sum + arr[i] ;
       arr.push(sum);
   }
@@ -37,13 +37,19 @@ console.log(fibonacci(1000));
 
 //the cache will be called recursively so no need to add it to our initial call ex fibMemo(20) is all we would run
 function fibMemo(index, cache) {
-  cache = cache || [] //if cache doesnt exist yet like in the first run then it will = [] otherwise it will = the number passed recursively
+  cache = cache || [] //if cache does not exist yet like in the first run then it will = [] otherwise it will = the number passed recursively
   if (cache[index]) return cache[index]; //BASE CASE if the cache at the index exists then return the cache at the index 
   else {
     if (index < 3) return 1;
     else cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    console.log(cache[index]);
   }
   return cache[index];
 }
 
-console.log(fibMemo(1000));
+console.log(fibMemo(30));
+//cache = []
+//cache[4] does not exist
+//cache[4] = fibMemo(3,[]) + fibMemo(2,[]) so cache[4] = cache[3] + cache[2]
+//                               fibMemo(2,[]) so cache[2] = 1
+//           cache[3] = fibMemo(2,[]) + fibMemo(1,[]) so cache[3] = 2
---
+++ sieveOfE.js
@@ -0,0 +1,10 @@
+//Sieve Of Eratosthenes
+//return all prime numbers up to our number in our array
+function sOE(num) {
+
+  console.log(numArr)
+}
+
+sOE(200);
+//[2,3,5,7,11,13,17,19]
+//Prime numbers are numbers with only two factors – themselves and 1. This means they cannot be divided by any other numbers without leaving a remainder.
