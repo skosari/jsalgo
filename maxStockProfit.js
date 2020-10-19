e6b4e65ee99d5e8f2682cdf43a039be77909850e
Siamak Kosari <skosari@protonmail.com>
2020-10-19 12:03:09 -0700

    maxStockProfit.js

---
+++ maxStockProfit.js
@@ -0,0 +1,8 @@
+// Our algorithm will determine what the max profit we could have made throughout the day
+// [32, 46, 26, 38, 40, 48, 42]
+//      buy@26     sell@48
+// return -1 if there is no profit to be made that day
+
+function maxStockProfit(priceArray) {
+
+}
