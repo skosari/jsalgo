d9bf460e865466df9da348148b72045b38a2d588
skosari <skosari@protonmail.com>
2020-10-19 13:06:34 -0700

    maxStockProfit.js working

--- maxStockProfit.js
+++ maxStockProfit.js
@@ -3,6 +3,26 @@
 //      buy@26     sell@48
 // return -1 if there is no profit to be made that day
 
-function maxStockProfit(priceArray) {
+function maxStockProfit(priceArr) {
+  var maxProfit = -1;
+  var buyPrice = 0;
+  var sellPrice = 0;
+  var changeBuyPrice = true;
+  for (var i = 0; i < priceArr.length; i++) {
+    if (changeBuyPrice) buyPrice = priceArr[i];
+    
+    sellPrice = priceArr[i+1];
 
+    if (sellPrice < buyPrice) {
+      changeBuyPrice = true;
+    }
+    else {
+      var tempProfit = sellPrice - buyPrice;
+      if (tempProfit > maxProfit) maxProfit = tempProfit;
+      changeBuyPrice = false;
+    }
+  }
+  return maxProfit;
 }
+
+console.log(maxStockProfit([32, 46, 26, 38, 40, 48, 42]))
